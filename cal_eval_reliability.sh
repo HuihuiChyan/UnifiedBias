0b4d9c084b5fb7f3a42d0d0d5d4f41fae346a289
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-26 12:02:17 +0800

    automatic push

--- cal_eval_reliability.sh
+++ cal_eval_reliability.sh
@@ -1,12 +1,12 @@
 export CUDA_VISIBLE_DEVICES=4
 
-python3 -u cal_eval_reliability.py \
-    --model-name "llama-2-13b-chat" \
-    --infer-mode "pairwise" \
-    --data-type "judgelm" \
-    --max-new-token 512
-
-# python3 -u eval_reliability.py \
+# python3 -u cal_eval_reliability.py \
 #     --model-name "llama-2-13b-chat" \
 #     --infer-mode "pairwise" \
-#     --data-type "pandalm"
+#     --data-type "judgelm" \
+#     --max-new-token 512
+
+python3 -u eval_reliability.py \
+    --model-name "llama-2-13b-chat" \
+    --infer-mode "pairwise" \
+    --data-type "judgelm"
