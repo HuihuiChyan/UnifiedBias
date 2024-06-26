754fcdc4b048fe3e8f6f579966c2cd45b01c886c
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-26 11:42:48 +0800

    automatic push

--- evaluate_bias.py
+++ evaluate_bias.py
@@ -163,7 +163,7 @@ def load_dataset(data_type, data_path = "./test_data"):
                 new_dataset.append(example)
         
         dataset = new_dataset
-        dataset = random.choice(dataset, k=1000)
+        dataset = random.sample(dataset, k=100)
 
     elif data_type == "pandalm":
         with open(os.path.join(data_path, "pandalm/testset-v1.json"), "r") as fin:
