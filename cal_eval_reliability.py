957929e92309a00ee5ad742d3e8e7f68e6231315
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-26 15:26:54 +0800

    automatic push

--- cal_eval_reliability.py
+++ cal_eval_reliability.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
     # 初始化结果字典
     results = {"Entropy": [], "Variance": [], "Logit": []}
 
-    model = AutoModelForCausalLM.from_pretrained(model_path).half().cuda()
+    model = AutoModelForCausalLM.from_pretrained(model_path, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto")
     model.eval()
 
     for i in tqdm(range(len(predictions)), desc="Calculating reliability score"):
