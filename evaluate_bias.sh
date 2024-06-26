132710cd4e353c690cee0e19fae3fa25ea9934e9
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-26 12:25:00 +0800

    automatic push

--- evaluate_bias.sh
+++ evaluate_bias.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=1,2,3,5
 python3 -u evaluate_bias.py \
-    --model-name "llama-2-13b-chat" \
+    --model-name "Meta-Llama-3-70B" \
     --infer-mode "pairwise" \
     --data-type "pandalm" \
     --max-new-token 1024
