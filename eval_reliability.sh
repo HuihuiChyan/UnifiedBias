5622bf0ce23f1a5ccb23f9268723745941a95173
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-26 17:48:47 +0800

    automatic push

--- eval_reliability.sh
+++ eval_reliability.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=3
 
 python3 -u eval_reliability.py \
-    --model-name "vicuna-13b" \
+    --model-name "Meta-Llama-3-70B-Instruct" \
     --infer-mode "pairwise" \
     --data-type "pandalm" \
-    --relia-type "pred"
+    --relia-type "eval"
