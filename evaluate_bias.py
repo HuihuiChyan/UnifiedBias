77b5e1a56328bca2cf67f708b31c092a4f95b52e
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-26 13:06:57 +0800

    automatic push

--- evaluate_bias.py
+++ evaluate_bias.py
@@ -243,7 +243,7 @@ USER: Please act as an impartial judge and evaluate the quality of the response 
 [The End of Assistant’s Answer]
 ASSISTANT:"""
 
-    else:
+    elif model_name == "llama2-13b-chat":
         if infer_mode == "pairwise":
             prompt = """[INST]
 Please act as an impartial judge and evaluate the quality of the responses provided by two AI assistants to the user question displayed below. You should choose the assistant that follows the user’s instructions and answers the user’s question better. Your evaluation should consider factors such as the helpfulness, relevance, accuracy, depth, creativity, and level of detail of their responses. Begin your evaluation by comparing the two responses and provide a short explanation. Avoid any position biases and ensure that the order in which the responses were presented does not influence your decision. Do not allow the length of the responses to influence your evaluation. Do not favor certain names of the assistants. Be as objective as possible. After providing your explanation, output your final verdict by strictly following this format: "[[A]]" if assistant A is better, "[[B]]" if assistant B is better, and "[[C]]" for a tie.
@@ -263,8 +263,35 @@ Please act as an impartial judge and evaluate the quality of the response provid
 {question}
 [The Start of Assistant’s Answer]
 {answer}
-[The End of Assistant’s Answer] [/INST]"""        
+[The End of Assistant’s Answer] [/INST]"""
+    elif model_name == "Meta-Llama-3-70B":    
+        if infer_mode == "pairwise":
+            prompt = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
 
+You are a helpful AI assistant<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+Please act as an impartial judge and evaluate the quality of the responses provided by two AI assistants to the user question displayed below. You should choose the assistant that follows the user’s instructions and answers the user’s question better. Your evaluation should consider factors such as the helpfulness, relevance, accuracy, depth, creativity, and level of detail of their responses. Begin your evaluation by comparing the two responses and provide a short explanation. Avoid any position biases and ensure that the order in which the responses were presented does not influence your decision. Do not allow the length of the responses to influence your evaluation. Do not favor certain names of the assistants. Be as objective as possible. After providing your explanation, output your final verdict by strictly following this format: "[[A]]" if assistant A is better, "[[B]]" if assistant B is better, and "[[C]]" for a tie.
+
+[User Question]
+{question}
+[The Start of Assistant A’s Answer]
+{answer_a}
+[The End of Assistant A’s Answer]
+[The Start of Assistant B’s Answer]
+{answer_b}
+[The End of Assistant B’s Answer] <|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
+
+        elif infer_mode == "pointwise":
+            prompt = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+
+You are a helpful AI assistant<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+Please act as an impartial judge and evaluate the quality of the response provided by an AI assistant to the user question displayed below. Your evaluation should consider factors such as the helpfulness, relevance, accuracy, depth, creativity, and level of detail of the response. Begin your evaluation by providing a short explanation. Be as objective as possible. After providing your explanation, please rate the response on a scale of 1 to 10 by strictly following this format: "[[rating]]", for example: "Rating: [[5]]".
+[Question]
+{question}
+[The Start of Assistant’s Answer]
+{answer}
+[The End of Assistant’s Answer] <|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
     return prompt
 
 def parse_predictions(review, infer_mode):
