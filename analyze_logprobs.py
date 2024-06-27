a7b5fa967fe116f0afc9023596ec28fcbe155538
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-27 21:18:12 +0800

    automatic push

--- analyze_logprobs.py
+++ analyze_logprobs.py
@@ -9,7 +9,7 @@ relia_file = f"output_data/{data_type}-{model_name}-{infer_mode}-ans-relia.json"
 heat_dict = defaultdict(lambda x:[])
 
 with open(relia_file, "r") as file_in:
-    results = json.loads(file_in)
+    results = json.load(file_in)
     logprobs = results["prompt_logprobs"]
     heat_dict = {}
     for logprob in logprobs:
