f8d6c32e71b9da0d55aadaf146a37def75ca8d19
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-27 14:25:52 +0800

    automatic push

--- cal_ans_reliability.sh
+++ cal_ans_reliability.sh
@@ -3,5 +3,5 @@ export CUDA_VISIBLE_DEVICES=2
 python3 -u cal_ans_reliability.py \
     --model-name "Mixtral-8x7B-Instruct-v0.1" \
     --infer-mode "pairwise" \
-    --data-type "pandalm" \
+    --data-type "verbosity" \
     --max-new-token 512
