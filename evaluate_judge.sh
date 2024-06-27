0d5679f0f2045979716f61c947356cad9b91b3cc
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-27 18:54:20 +0800

    automatic push

--- evaluate_judge.sh
+++ evaluate_judge.sh
@@ -1,6 +1,7 @@
-export CUDA_VISIBLE_DEVICES=1,2,3,4
+export CUDA_VISIBLE_DEVICES=2,3,4,5
+
 python3 -u evaluate_judge.py \
     --model-name "mixtral-8x7b-instruct-v0.1" \
     --infer-mode "pairwise" \
-    --data-type "self" \
+    --data-type "verbosity" \
     --max-new-token 1024
