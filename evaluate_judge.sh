c0792ca6c183ac580a27e42c80c9cc329b318708
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-27 20:32:11 +0800

    automatic push

--- evaluate_judge.sh
+++ evaluate_judge.sh
@@ -3,5 +3,5 @@ export CUDA_VISIBLE_DEVICES=1,7
 python3 -u evaluate_judge.py \
     --model-name "llama-2-70b-chat" \
     --infer-mode "pairwise" \
-    --data-type "self" \
+    --data-type "verbosity" \
     --max-new-token 1024
