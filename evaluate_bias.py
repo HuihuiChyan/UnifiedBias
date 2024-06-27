84553f7e77d34e22496aaff3347533c1a7a7d806
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-27 10:41:26 +0800

    automatic push

--- evaluate_bias.py
+++ evaluate_bias.py
@@ -248,7 +248,7 @@ USER: Please act as an impartial judge and evaluate the quality of the response 
 [The End of Assistant’s Answer]
 ASSISTANT:"""
 
-    elif model_name == "llama2-13b-chat":
+    elif model_name == "llama-2-13b-chat":
         if infer_mode == "pairwise":
             prompt = """[INST]
 Please act as an impartial judge and evaluate the quality of the responses provided by two AI assistants to the user question displayed below. You should choose the assistant that follows the user’s instructions and answers the user’s question better. Your evaluation should consider factors such as the helpfulness, relevance, accuracy, depth, creativity, and level of detail of their responses. Begin your evaluation by comparing the two responses and provide a short explanation. Avoid any position biases and ensure that the order in which the responses were presented does not influence your decision. Do not allow the length of the responses to influence your evaluation. Do not favor certain names of the assistants. Be as objective as possible. After providing your explanation, output your final verdict by strictly following this format: "[[A]]" if assistant A is better, "[[B]]" if assistant B is better, and "[[C]]" for a tie.
