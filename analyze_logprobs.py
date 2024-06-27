d0ba33d06b5e955225f93e603a92525b6ab5f4fe
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-27 21:17:37 +0800

    automatic push

--- analyze_logprobs.py
+++ analyze_logprobs.py
@@ -13,6 +13,10 @@ with open(relia_file, "r") as file_in:
     for logprob in logprobs:
         for i, lp in enumerate(logprob):
             heat_dict[str(i)].append(lp)
+    
+    heat_list = []
     for i in range(10000):
         if heat_dict[i] != []:
-            heat_dict[str(i)] = sum(heat_dict[str(i)]) / len(heat_dict[str(i)])
+            heat_list = sum(heat_dict[str(i)]) / len(heat_dict[str(i)])
+
+    print(heat_list)
