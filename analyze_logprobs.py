9710aa450118566dac898743c10b4c440c1fbb0e
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-27 21:15:34 +0800

    automatic push

--- analyze_logprobs.py
+++ analyze_logprobs.py
@@ -1,7 +1,15 @@
+from collections import defaultdict
 data_type = "self"
 model_name = "llama-2-70b-chat"
 infer_mode = "pairwise"
 relia_file = f"output_data/{data_type}-{model_name}-{infer_mode}-ans-relia.json"
+
+heat_dict = defaultdict(lambda x:[])
+
 with open(relia_file, "r") as file_in:
     results = json.loads(file_in)
-    import pdb;pdb.set_trace()
+    logprobs = results["prompt_logprobs"]
+    heat_dict = {}
+    for logprob in logprobs:
+        for i, lp in enumerate(logprob):
+            heat_dict[str(i)] 
