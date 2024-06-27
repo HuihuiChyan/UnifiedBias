2d4dbad462b552fd3c026016743601bd4ab48b57
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-27 11:12:07 +0800

    automatic push

--- cal_eval_reliability.sh
+++ cal_eval_reliability.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=2
 
 python3 -u cal_eval_reliability.py \
-    --model-name "llama-2-13b-chat" \
+    --model-name "Meta-Llama-3-70B-Instruct" \
     --infer-mode "pairwise" \
     --data-type "pandalm" \
     --max-new-token 512
