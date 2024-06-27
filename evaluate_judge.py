e8442100aef2f52c4d54c997dfd7f4000c8ede79
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-27 14:49:56 +0800

    automatic push

--- evaluate_judge.py
+++ evaluate_judge.py
@@ -78,7 +78,7 @@ def batched_generation(
 ):
     print("Start load VLLM model!")
     import vllm
-    model = vllm.LLM(model=model_path, tensor_parallel_size=torch.cuda.device_count(), dtype="bfloat16", gpu_memory_utilization=0.9)
+    model = vllm.LLM(model=model_path, tensor_parallel_size=torch.cuda.device_count(), gpu_memory_utilization=0.9)
     sampling_params = vllm.SamplingParams(
         temperature=temperature,
         max_tokens=max_new_token,
