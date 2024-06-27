34d85d46ed679e52b942da7c291e62786a20b7a2
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-27 21:41:24 +0800

    automatic push

--- analyze_logprobs.py
+++ analyze_logprobs.py
@@ -1,4 +1,6 @@
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 from collections import defaultdict
 
 data_type = "self"
@@ -6,19 +8,23 @@ model_name = "llama-2-70b-chat"
 infer_mode = "pairwise"
 relia_file = f"output_data/{data_type}-{model_name}-{infer_mode}-ans-relia.json"
 
-heat_dict = defaultdict(lambda x:[])
+heat_dict = defaultdict(lambda :[])
 
 with open(relia_file, "r") as file_in:
     results = json.load(file_in)
     logprobs = results["prompt_logprobs"]
-    heat_dict = {}
     for logprob in logprobs:
         for i, lp in enumerate(logprob):
             heat_dict[str(i)].append(lp)
-    
+
     heat_list = []
     for i in range(10000):
-        if heat_dict[i] != []:
-            heat_list = sum(heat_dict[str(i)]) / len(heat_dict[str(i)])
+        for j in range(10):
+            if heat_dict[str(i*10+j)] != []:
+                heat_list.append(sum(heat_dict[str(i)]) / len(heat_dict[str(i)]))
 
-    print(heat_list)
+import pdb;pdb.set_trace()
+heat_list = np.array(heat_list)
+posi_list = np.arange(len(heat_list))
+plt.plot(posi_list, heat_list)  # Plot the chart
+plt.show()  # display
