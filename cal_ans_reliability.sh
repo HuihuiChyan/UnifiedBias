336fa901e0f9f0b364b918aeb9624ae53f9468a8
huanghui_dxm <huanghui@duxiaoman.com>
2024-06-28 10:45:19 +0800

    automatic push

--- cal_ans_reliability.sh
+++ cal_ans_reliability.sh
@@ -3,5 +3,5 @@ export CUDA_VISIBLE_DEVICES=2,3,4,5
 python3 -u cal_ans_reliability.py \
     --model-name "llama-2-70b-chat" \
     --infer-mode "pairwise" \
-    --data-type "verbo_same" \
+    --data-type "same_verbo" \
     --max-new-token 512
